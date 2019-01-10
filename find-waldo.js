e4452fb1448da8ce0aecabb6f3d26056e1e276ee
Aedan <aedan.giffin@gmail.com>
2019-01-10 17:54:23 +0000

    implement forEach() method to find multiple waldo 's and logs each element where they are found

--- find-waldo.js
+++ find-waldo.js
@@ -1,14 +1,20 @@
 // The second argument/parameter is expected to be a function
+
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  var currentElement = 0;
+  arr.forEach(function(name) {
+    if(name === "Waldo") {
+      found(currentElement);
     }
-  }
+    currentElement++;
+  });
 }
+
+
 
 function actionWhenFound(i) {
-  console.log("Found him at index: " + i);
+  console.log("Found him at index: " + i + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston", "Waldo", "Waldo", "Waldo"], actionWhenFound);
